24260cc4b1d25b7c0c21a6544b625ad7526ef7c2
Noe <dsdlnoe@gmail.com>
2023-02-06 18:18:41 -0500

    Added function to answer last question

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from itertools import product
 import re
+import pandas as pd
 
 
 class HanoiTower:
@@ -39,7 +40,9 @@ class HanoiTower:
                 self.diff.append(j)
                 self.idx = [a.index(i), b.index(j)]
 
-    def create_tower(self):
+    def create_tower(self, shortest_path=True, plot_diagram=True):
+        if (shortest_path not in [True, False]) or (shortest_path not in [True, False]):
+            raise ValueError("Options must be boolean")
         for node in self.nodes:
             self.graph.add_node(''.join(node), room=1500)
         for first in self.graph.nodes:
@@ -55,37 +58,41 @@ class HanoiTower:
                                 second[i] not in (second_lst[i + 1:])):
                             self.graph.add_edge(first, second)
                         i += 1
-        self.find_shortest_path()
+        if shortest_path:
+            self.find_shortest_path(plot_diagram=plot_diagram)
+        elif plot_diagram:
+            self.plot_tower()
 
-    def find_shortest_path(self):
+    def find_shortest_path(self, plot_diagram=True):
         self.short_path_nodes = nx.shortest_path(self.graph, self.start, self.end)
-        i = 0
-        while i < len(self.short_path_nodes)-1:
-            if i == 0:
-                self.short_path_edges.append(('start', self.short_path_nodes[i + 1]))
-                self.short_path_edges.append((self.short_path_nodes[i + 1], 'start'))
-            elif i == len(self.short_path_nodes)-2:
-                self.short_path_edges.append((self.short_path_nodes[i], 'end'))
-                self.short_path_edges.append(('end', self.short_path_nodes[i]))
-            else:
-                self.short_path_edges.append((self.short_path_nodes[i], self.short_path_nodes[i + 1]))
-                self.short_path_edges.append((self.short_path_nodes[i+1], self.short_path_nodes[i]))
-            i += 1
-        for node in self.short_path_nodes:
-            if node == self.end:
-                self.node_color_map.append('red')
-                self.short_path_nodes[-1] = 'end'
-            elif node == self.start:
-                self.node_color_map.append('green')
-                self.short_path_nodes[0] = 'start'
-            elif node in self.short_path_nodes:
-                self.node_color_map.append('orange')
+        if plot_diagram:
+            i = 0
+            while i < len(self.short_path_nodes)-1:
+                if i == 0:
+                    self.short_path_edges.append(('start', self.short_path_nodes[i + 1]))
+                    self.short_path_edges.append((self.short_path_nodes[i + 1], 'start'))
+                elif i == len(self.short_path_nodes)-2:
+                    self.short_path_edges.append((self.short_path_nodes[i], 'end'))
+                    self.short_path_edges.append(('end', self.short_path_nodes[i]))
+                else:
+                    self.short_path_edges.append((self.short_path_nodes[i], self.short_path_nodes[i + 1]))
+                    self.short_path_edges.append((self.short_path_nodes[i+1], self.short_path_nodes[i]))
+                i += 1
+            for node in self.short_path_nodes:
+                if node == self.end:
+                    self.node_color_map.append('red')
+                    self.short_path_nodes[-1] = 'end'
+                elif node == self.start:
+                    self.node_color_map.append('green')
+                    self.short_path_nodes[0] = 'start'
+                elif node in self.short_path_nodes:
+                    self.node_color_map.append('orange')
 
-        for edge in self.graph.edges:
-            if edge in self.short_path_edges:
-                self.edge_color_map.append("green")
-        self.graph = nx.relabel_nodes(self.graph, {self.start: 'start', self.end: 'end'})
-        self.plot_tower()
+            for edge in self.graph.edges:
+                if edge in self.short_path_edges:
+                    self.edge_color_map.append("green")
+            self.graph = nx.relabel_nodes(self.graph, {self.start: 'start', self.end: 'end'})
+            self.plot_tower()
 
     def plot_tower(self):
         iter_graph = 10000
@@ -93,8 +100,15 @@ class HanoiTower:
         if self.disks == 6:
             fig = plt.figure(1, figsize=(50, 50), dpi=60)
             size = 700
-            iter_graph = 100
-        pos = nx.spring_layout(self.graph, iterations=iter_graph)
+            iter_graph = 50
+            print("CREATING GRAPH...")
+            pos = nx.spring_layout(self.graph, iterations=iter_graph)
+        elif self.towers == 4:
+            fig = plt.figure(1, figsize=(15, 15), dpi=60)
+            size = 600
+            pos = nx.circular_layout(self.graph)
+        else:
+            pos = nx.spring_layout(self.graph, iterations=iter_graph)
         nx.draw_networkx_labels(
             self.graph,
             pos,
@@ -105,8 +119,23 @@ class HanoiTower:
         nx.draw(self.graph, pos, nodelist=self.short_path_nodes, node_color=self.node_color_map,
                 edgelist=self.short_path_edges, edge_color="green", width=3, node_size=size)
         plt.show()
+        # plt.savefig("3_4")
+
+    def get_metrics(self):
+        print(len(self.short_path_nodes))
+        # print(nx.adjacency_matrix(self.graph))
+        # print(nx.to_pandas_adjacency(self.graph))
+        # print(nx.incidence_matrix(self.graph))
+        # print(nx.eigenvector_centrality(self.graph))
+        print(nx.is_eulerian(self.graph))
 
 
-# test = HanoiTower(6, 3, '111111', '333333')
-test = HanoiTower(3, 3, '111', '333')
-test.create_tower()
+
+# Q = HanoiTower(6, 3, '111111', '333333')
+Q = HanoiTower(3, 3, '111', '333')
+# Q = HanoiTower(3, 4, '111', '444')
+# Q = HanoiTower(6, 4, '133122', '333333')
+
+
+Q.create_tower(shortest_path=True, plot_diagram=False)
+Q.get_metrics()
