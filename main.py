fbbdbe18a290147db73372bc4a1a18b7ca49b5af
Noe <dsdlnoe@gmail.com>
2023-02-06 11:27:09 -0500

    Make large graphs more readable

--- main.py
+++ main.py
@@ -88,18 +88,21 @@ class HanoiTower:
         self.plot_tower()
 
     def plot_tower(self):
-        pos = nx.spring_layout(self.graph, iterations=1000)
+        pos = nx.spring_layout(self.graph, iterations=100)
+        # pos = nx.circular_layout(self.graph)
+        fig = plt.figure(1, figsize=(50, 50), dpi=60)
         nx.draw_networkx_labels(
             self.graph,
             pos,
-            font_size=8,
+            font_size=10,
             font_family="sans-serif",
         )
-        nx.draw(self.graph, pos, with_labels=False, node_color="grey", edge_color="grey")
+        nx.draw(self.graph, pos, with_labels=False, node_color="grey", edge_color="grey", node_size=700)
         nx.draw(self.graph, pos, nodelist=self.short_path_nodes, node_color=self.node_color_map,
-                edgelist=self.short_path_edges, edge_color="green", width=3)
+                edgelist=self.short_path_edges, edge_color="green", width=3, node_size=700)
         plt.show()
+        # plt.savefig("6_6.png")
 
 
-test = HanoiTower(3, 3, '111', '333')
+test = HanoiTower(6, 3, '111111', '333333')
 test.create_tower()
